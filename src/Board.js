cffa7f5f75736c501c86bedb8f1c960052c3a177
xaviguasch <xaviguasch@gmail.com>
2019-05-04 18:20:51 +0200

    flipped the neighbour cells and updated the state

--- src/Board.js
+++ src/Board.js
@@ -78,6 +78,10 @@ class Board extends Component {
     }
 
     flipCell(y, x)
+    flipCell(y, x - 1)
+    flipCell(y, x + 1)
+    flipCell(y - 1, x)
+    flipCell(y + 1, x)
 
     // TODO: flip this cell and the cells around it
 
